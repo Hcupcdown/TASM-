c5ed90f87347da0e0b0e1f0d92b7d86165c722bb
Hcupcdown <45099559+Hcupcdown@users.noreply.github.com>
2018-11-29 19:40:09 +0800

    add first interface experiment

--- S_TRY.asm
+++ S_TRY.asm
@@ -1,10 +1,9 @@
 .486
 DATA    SEGMENT USE16
-MESG    DB  'B16040530',0DH,0AH,'$'
+MESG    DB  '23:59:50',0DH,'$'
 OLD1C   DD  ?
-OLD09	DD	?
-COUNT   DB  10
 ICOUNT  DB  18
+COUNT0  DB  1
 DATA    ENDS
 CODE    SEGMENT USE16
         ASSUME  CS:CODE, DS:DATA
@@ -14,7 +13,7 @@ BEG:    MOV AX, DATA
         CALL    READ1C
         CALL    WRITE1C
         STI
-SCAN:   CMP     COUNT,	0
+SCAN:   CMP     COUNT0,	0
         JNZ     SCAN
         CLI
         CALL    RESET1C
@@ -27,16 +26,40 @@ SERVICE PROC
         PUSH    DS
         MOV     AX, DATA
         MOV     DS, AX
-		MOV		AH,	01H
-		INT		16H
-		JZ		ENDCOUNT
-		MOV		COUNT,	0
-ENDCOUNT:
         DEC     ICOUNT
         JNZ     EXIT
-		LEA		DX,	MESG
-		MOV		AH,	09H
-		INT		21H
+        MOV     BX, OFFSET  MESG
+        INC     BYTE PTR [BX+7]
+        CMP     BYTE PTR [BX+7], 3AH
+        JNZ     NEXT0
+        MOV     BYTE PTR [BX+7], 30H
+        INC     BYTE PTR [BX+6]
+        CMP     BYTE PTR [BX+6], 36H
+        JNZ     NEXT0
+        MOV     BYTE PTR [BX+6], 30H
+        INC     BYTE PTR [BX+4]
+        CMP     BYTE PTR [BX+4], 3AH
+        JNZ     NEXT0
+        MOV     BYTE PTR [BX+4], 30H
+        INC     BYTE PTR [BX+3]
+        CMP     BYTE PTR [BX+3], 36H
+        JNZ     NEXT0
+        MOV     BYTE PTR [BX+3], 30H
+        INC     BYTE PTR [BX+1]
+        CMP     BYTE PTR [BX],  32H
+        JZ      JUDGE
+JUDGE:  CMP     BYTE PTR [BX+1], 34H
+        JNZ     NEXT1
+        MOV     BYTE PTR [BX+1],30H
+        MOV     BYTE PTR [BX],  30H
+NEXT1:  CMP     BYTE PTR [BX+1], 3AH
+        JNZ     NEXT0
+        INC     BYTE PTR [BX]
+        MOV     BYTE PTR [BX+1],30H
+
+NEXT0:  MOV     AH, 9
+        LEA     DX, MESG
+        INT     21H
         MOV     ICOUNT, 18
 EXIT:   POP     DS
         POPA
@@ -69,6 +92,5 @@ RESET1C PROC
         INT     21H
         RET
 RESET1C ENDP
-
 CODE    ENDS
         END     BEG
